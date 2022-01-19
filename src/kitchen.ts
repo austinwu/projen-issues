de01b766df6936fa489b2dab1e10a0b84b53274e
Austin Wu <austinwu32@gmail.com>
2022-01-19 15:21:16 -0500

    feat: kitchen can bake bread

--- src/kitchen.ts
+++ src/kitchen.ts
@@ -2,4 +2,8 @@ export class Kitchen {
   public assembleSandwich(...ingredients: string[]) {
     return ["bread", ...ingredients, "bread"];
   }
+
+  public bakeBread(dough: string) {
+    return dough.toUpperCase();
+  }
 }
